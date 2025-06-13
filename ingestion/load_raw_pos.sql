562aa19e36e4454d892f07a99a1ca33501c29f7b
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-13 16:26:08 -0600

    Create load_raw_pos.sql

---
+++ ingestion/load_raw_pos.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE STORAGE INTEGRATION my_s3_integration
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = S3
+  ENABLED = TRUE
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::457151801201:role/snowflake_role'
+  STORAGE_ALLOWED_LOCATIONS = ('s3://snowflake-northwind/');
