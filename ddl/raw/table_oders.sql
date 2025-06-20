ce583c252be47f07f2386646c3adf17a1af002e8
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-20 15:14:34 -0600

    Create table_oders.sql

---
+++ ddl/raw/table_oders.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE TABLE orders (
+    order_id INT,
+    customer_id VARCHAR(5),
+    employee_id INT,
+    order_date DATE,
+    required_date DATE,
+    shipped_date DATE,
+    ship_via INT,
+    freight NUMBER(10, 4),
+    ship_name VARCHAR,
+    ship_address VARCHAR,
+    ship_city VARCHAR,
+    ship_region VARCHAR,
+    ship_postal_code VARCHAR,
+    ship_country VARCHAR
+);
