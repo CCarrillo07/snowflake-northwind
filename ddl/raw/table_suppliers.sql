363f4deec0740e08c4ede5534d17270db86c6187
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-20 15:15:39 -0600

    Create table_suppliers.sql

---
+++ ddl/raw/table_suppliers.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE suppliers (
+    supplier_id INT,
+    company_name VARCHAR,
+    contact_name VARCHAR,
+    contact_title VARCHAR,
+    address VARCHAR,
+    city VARCHAR,
+    region VARCHAR,
+    postal_code VARCHAR,
+    country VARCHAR,
+    phone VARCHAR,
+    fax VARCHAR,
+    homepage VARCHAR
+);
