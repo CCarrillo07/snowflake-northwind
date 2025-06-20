41bc982192046802b2c031376e7c02175de77d84
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-20 15:11:00 -0600

    Create customers.sql

---
+++ ddl/raw/customers.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE customers (
+    customer_id VARCHAR(5),
+    company_name VARCHAR,
+    contact_name VARCHAR,
+    contact_title VARCHAR,
+    address VARCHAR,
+    city VARCHAR,
+    region VARCHAR,
+    postal_code VARCHAR,
+    country VARCHAR,
+    phone VARCHAR,
+    fax VARCHAR
+);
