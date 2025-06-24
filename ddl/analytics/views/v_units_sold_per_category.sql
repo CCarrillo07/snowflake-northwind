c3cd7fa1a51a987f61f4a827368aeac5efec90ff
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-24 15:39:03 -0600

    Update v_units_sold_per_category.sql

--- ddl/analytics/views/v_units_sold_per_category.sql
+++ ddl/analytics/views/v_units_sold_per_category.sql
@@ -1,9 +1,10 @@
 -- Units Sold per Category (simple GROUP BY)
 CREATE OR REPLACE VIEW analytics.v_units_sold_per_category AS
 SELECT 
-    category_id, 
-    category_name, 
-    SUM(quantity) AS total_units_sold
-FROM analytics.fact_order_lines
-GROUP BY category_id, category_name
+    f.category_id, 
+    c.category_name, 
+    SUM(f.quantity) AS total_units_sold
+FROM analytics.fact_order_lines f
+JOIN analytics.dim_categories c ON f.category_id = c.category_id
+GROUP BY f.category_id, c.category_name
 ORDER BY total_units_sold DESC;
