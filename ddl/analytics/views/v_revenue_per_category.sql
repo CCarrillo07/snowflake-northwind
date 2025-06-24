c9ca4b98593da4197112dbf74c93f24468d86824
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-24 15:38:14 -0600

    Update v_revenue_per_category.sql

--- ddl/analytics/views/v_revenue_per_category.sql
+++ ddl/analytics/views/v_revenue_per_category.sql
@@ -1,9 +1,10 @@
 -- Revenue per Category (simple GROUP BY)
 CREATE OR REPLACE VIEW analytics.v_revenue_per_category AS
 SELECT 
-    category_id, 
-    category_name, 
-    SUM(line_total) AS total_revenue
-FROM analytics.fact_order_lines
-GROUP BY category_id, category_name
+    f.category_id, 
+    c.category_name, 
+    SUM(f.line_total) AS total_revenue
+FROM analytics.fact_order_lines f
+JOIN analytics.dim_categories c ON f.category_id = c.category_id
+GROUP BY f.category_id, c.category_name
 ORDER BY total_revenue DESC;
