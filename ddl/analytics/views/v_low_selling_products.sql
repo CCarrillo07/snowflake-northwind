06a73240381025a9d78bbef872d891de879d42d9
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-24 15:38:01 -0600

    Update v_low_selling_products.sql

--- ddl/analytics/views/v_low_selling_products.sql
+++ ddl/analytics/views/v_low_selling_products.sql
@@ -1,11 +1,12 @@
 -- Low Selling Products (GROUP BY + HAVING)
 CREATE OR REPLACE VIEW analytics.v_low_selling_products AS
 SELECT
-    product_id,
-    product_name,
-    SUM(quantity) AS total_units_sold,
-    SUM(line_total) AS total_revenue
-FROM analytics.fact_order_lines
-GROUP BY product_id, product_name
-HAVING SUM(line_total) < 1000
+    f.product_id,
+    p.product_name,
+    SUM(f.quantity) AS total_units_sold,
+    SUM(f.line_total) AS total_revenue
+FROM analytics.fact_order_lines f
+JOIN analytics.dim_products p ON f.product_id = p.product_id
+GROUP BY f.product_id, p.product_name
+HAVING SUM(f.line_total) < 1000
 ORDER BY total_revenue ASC;
