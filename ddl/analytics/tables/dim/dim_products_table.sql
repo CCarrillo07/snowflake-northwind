705bca640147b8ab4367226dc97669b40208c07b
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-24 13:32:28 -0600

    Update dim_products_table.sql

--- ddl/analytics/tables/dim/dim_products_table.sql
+++ ddl/analytics/tables/dim/dim_products_table.sql
@@ -1,3 +1,3 @@
 CREATE OR REPLACE TABLE analytics.dim_products AS
 SELECT DISTINCT product_id, product_name, category_id, category_name
-FROM transformation.order_line_details;
+FROM harmonized.order_line_details;
