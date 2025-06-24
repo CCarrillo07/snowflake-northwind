27e7e4b3c970e01e64e4d551e09775d4d2eea469
CCarrillo07 <61401825+CCarrillo07@users.noreply.github.com>
2025-06-24 15:38:44 -0600

    Update v_top10_products_by_revenue.sql

--- ddl/analytics/views/v_top10_products_by_revenue.sql
+++ ddl/analytics/views/v_top10_products_by_revenue.sql
@@ -2,14 +2,17 @@
 CREATE OR REPLACE VIEW analytics.v_top10_products_by_revenue AS
 WITH ranked_products AS (
   SELECT 
-    product_id,
-    product_name,
-    SUM(line_total) AS total_revenue,
-    RANK() OVER (ORDER BY SUM(line_total) DESC) AS revenue_rank
-  FROM analytics.fact_order_lines
-  GROUP BY product_id, product_name
+    f.product_id,
+    SUM(f.line_total) AS total_revenue,
+    RANK() OVER (ORDER BY SUM(f.line_total) DESC) AS revenue_rank
+  FROM analytics.fact_order_lines f
+  GROUP BY f.product_id
 )
-SELECT product_id, product_name, total_revenue
-FROM ranked_products
-WHERE revenue_rank <= 10
-ORDER BY revenue_rank;
+SELECT 
+    r.product_id, 
+    p.product_name, 
+    r.total_revenue
+FROM ranked_products r
+JOIN analytics.dim_products p ON r.product_id = p.product_id
+WHERE r.revenue_rank <= 10
+ORDER BY r.revenue_rank;
